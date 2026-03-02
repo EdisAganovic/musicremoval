ef3ba580c6ce7bf5e72513e1a0728ab241cbe8d7
Edis Aganović <edis.aganovic@gmail.com>
2026-03-02 19:27:41 +0100

    0.0.7

---
+++ frontend/src/config.js
@@ -0,0 +1,11 @@
+/**
+ * Application Configuration
+ * 
+ * Central place for app-wide constants.
+ * Version should match pyproject.toml version.
+ */
+export const APP_VERSION = '0.0.8';
+
+export const APP_NAME = 'Audio Splitter Pro';
+
+export const BACKEND_URL = 'http://127.0.0.1:5170';
